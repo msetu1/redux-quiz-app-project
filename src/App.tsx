1914555fe83a3766c098515361fbce5440490373
msetu1 <msetu5763@gmail.com>
2025-01-07 20:10:20 +0600

    code update

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,15 @@
 import Question from "./home/Question";
+import QuizSummary from "./home/QuizSummary";
+import { useAppSelector } from "./redux/hooks";
 
 function App() {
+  const { quizComplete } = useAppSelector((state) => state.quiz);
   return (
     <div className="min-h-screen flex items-center justify-center">
-    <div>
-    <h2 className="text-center font-bold text-5xl mb-10">Quiz apps !!</h2>
-    <Question />
-    </div>
+      <div>
+        <h2 className="text-center font-bold text-5xl mb-10">Quiz apps !!</h2>
+        {!quizComplete ? <Question /> : <QuizSummary />}
+      </div>
     </div>
   );
 }
--- src/home/QuizControl.tsx
+++ src/home/QuizControl.tsx
@@ -1,5 +1,6 @@
 import { Button } from "@/components/ui/button";
 import {
+  completeQuiz,
   nextQuestion,
   previousQuestion,
 } from "@/redux/features/quiz/quizSlice";
@@ -9,6 +10,7 @@ const QuizControl = () => {
   const { question, currentQuestionIndex, userAnswer } = useAppSelector(
     (state) => state.quiz
   );
+  const isAnswerSelected = userAnswer[currentQuestionIndex] !== null;
   const dispatch = useAppDispatch();
 
   const handleNextQuestion = () => {
@@ -17,6 +19,13 @@ const QuizControl = () => {
   const handlePreviousQuestion = () => {
     dispatch(previousQuestion());
   };
+
+  const handleCompleteIndex = () => {
+    dispatch(completeQuiz());
+  };
+
+  const isCompleteQuiz =
+    isAnswerSelected || currentQuestionIndex !== question.length - 1;
   return (
     <div className="flex items-center justify-between mt-10 ">
       <Button
@@ -25,7 +34,16 @@ const QuizControl = () => {
       >
         Previous
       </Button>
-      <Button onClick={handleNextQuestion}>Next</Button>
+      {currentQuestionIndex < question.length - 1 && (
+        <Button disabled={!isAnswerSelected} onClick={handleNextQuestion}>
+          Next
+        </Button>
+      )}
+      {currentQuestionIndex === question.length - 1 && (
+        <Button disabled={!isCompleteQuiz} onClick={handleCompleteIndex}>
+          Complete Quiz
+        </Button>
+      )}
     </div>
   );
 };
