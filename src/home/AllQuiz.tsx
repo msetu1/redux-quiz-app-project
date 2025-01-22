d84feb99f4d21215c05b10fdc245d7469cb3a455
msetu1 <msetu5763@gmail.com>
2025-01-22 20:26:38 +0600

    code update

--- src/home/AllQuiz.tsx
+++ src/home/AllQuiz.tsx
@@ -1,8 +1,11 @@
+import { useGetAllQuizQuery } from "@/redux/api/quizApi";
 
 const AllQuiz = () => {
+    const {data,isLoading}=useGetAllQuizQuery(undefined);
+    console.log(data)
     return (
         <div className="mb-4">
-            serftgyhuj
+            All Quiz
         </div>
     );
 };
--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,10 +1,15 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { quizSlice } from "./features/quiz/quizSlice";
+import { quizApi } from "./api/quizApi";
 
 export const store = configureStore({
   reducer: {
     quiz: quizSlice.reducer,
+    [quizApi.reducerPath]:quizApi.reducer
   },
+  middleware:(getDefaultMiddleware)=>{
+    return getDefaultMiddleware().concat(quizApi.middleware)
+  }
 });
 
 export type RootState = ReturnType<typeof store.getState>;
